27f8c460afa846496c3e8c5b24c4be9b9ddb3c78
Drezze <145409315+Drezze@users.noreply.github.com>
2023-11-13 18:59:45 -0800

    GitIgnore

---
+++ testproj/GitIgnore.cs
@@ -0,0 +1,47 @@
+﻿# Unity specific
+[Ll] ibrary /
+[Tt]emp /
+[Oo]bj /
+[Bb]uild /
+[Bb]uilds /
+[Ll]ogs /
+[Uu]ser[Ss]ettings /
+
+# MemoryCaptures can get excessive in size.
+*.mem
+
+# Asset meta data should only be ignored in specific cases.
+! *.meta
+
+# Auto generated file on build
+/ Builds /
+
+# macOS-specific files
+.DS_Store
+.AppleDouble
+.LSOverride
+
+# Windows-specific files
+Thumbs.db
+ehthumbs.db
+
+# Visual Studio files
+*.csproj
+*.sln
+*.suo
+*.tmp
+*.user
+*.userprefs
+*.pidb
+*.booproj
+*.ide
+
+# Rider files
+.idea /
+*.sln.iml
+
+# macOS directory attributes
+__MACOSX /
+
+# Linux and MacOS environment files
+*.pidb.meta
